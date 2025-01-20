bc82a1c4d067d2a9603e7552495888e5fe79a0a3
zangula-dev <simalc@zangula.com>
2025-01-20 13:12:56 +0530

    Add

--- events-backend/app.js
+++ events-backend/app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
